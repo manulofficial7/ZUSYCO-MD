76dbb811025d2d8c3faf8dc7c4eed2aae19b557e
manulofficial7 <157870475+manulofficial7@users.noreply.github.com>
2024-03-10 19:51:51 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -55,7 +55,7 @@ module.exports = {
   BOT_OFFLINE : true,
   INBOX_BLOCK : true,
   AUTO_REACT : true,
-  OWNER_REACT : '🤵‍♂️',
+  OWNER_REACT : '⚖️',
   AUTO_BGM : true,
   BGM : {
     "chaio" : "https://github.com/MR-NIMA-X/ZUSYCO-DB/raw/main/media/audios/ZUSYCO-caio.mp3",
